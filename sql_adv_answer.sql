6bf9b05c0940f5e5c598c377756f75738d2f3411
rhombus0516 <karaya0913@gmail.com>
2024-07-03 22:11:10 +0900

    [Add]答え1~4

--- sql_adv_answer.sql
+++ sql_adv_answer.sql
@@ -1,7 +1,15 @@
 /* 問1 */
+select group_name as 'グループ', MIN(ranking) as 'ランキング最上位', MAX(ranking) as 'ランキング最下位' from countries group by group_name;
+
 /* 問2 */
+select AVG(height) as '平均身長', AVG(weight) as '平均体重' from players where 1=1 and position = 'GK';
+
 /* 問3 */
+select countries.name as '国名', AVG(height) as '平均身長' from countries join players on countries.id = players.country_id group by countries.id, countries.name order by avg(height) desc;
+
 /* 問4 */
+select (select c.name from countries c where p.country_id = c.id) as 'kunimei', avg(height) as 'sincho' from players p group by p.country_id order by avg(p.height) desc;
+
 /* 問5 */
 /* 問6 */
 /* 問7 */
