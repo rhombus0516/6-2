193734a39ad8a09fbadc12afe14fcffc7d57e1ef
rhombus0516 <karaya0913@gmail.com>
2024-07-05 22:20:07 +0900

    [Add]答え5~8

--- sql_adv_answer.sql
+++ sql_adv_answer.sql
@@ -11,9 +11,17 @@ select countries.name as '国名', AVG(height) as '平均身長' from countries 
 select (select c.name from countries c where p.country_id = c.id) as 'kunimei', avg(height) as 'sincho' from players p group by p.country_id order by avg(p.height) desc;
 
 /* 問5 */
+select kickoff, my_country.name, enemy_country.name from pairings join countries as my_country on pairings.my_country_id = my_country.id join countries enemy_country on pairings.enemy_country_id = enemy_country.id order by kickoff;
+
 /* 問6 */
+select name as '名前', position as 'ポジション', club as '所属クラブ', (select COUNT(g.id) from goals g where g.player_id = p.id) as 'ゴール数' from players p order by 'ゴール数' desc;
+
 /* 問7 */
+select name, position, club, COUNT(g.id) as 'ゴール数' from players p LEFT JOIN goals g ON g.player_id = p.id group by p.id, p.name, p.position, p.club order by ゴール数 desc;
+
 /* 問8 */
+select players.position as 'ポジション', count(goals.id) as 'ゴール数' from goals left join players on goals.player_id = players.id group by players.position;
+
 /* 問9 */
 /* 問10 */
 /* 問11 */
