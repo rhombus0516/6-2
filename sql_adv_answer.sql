7e5d0c473e7b7cf671d78fcbb581777dbd9a94d7
rhombus0516 <karaya0913@gmail.com>
2024-07-06 21:21:27 +0900

    [Add]答え8~24

--- sql_adv_answer.sql
+++ sql_adv_answer.sql
@@ -23,18 +23,51 @@ select name, position, club, COUNT(g.id) as 'ゴール数' from players p LEFT J
 select players.position as 'ポジション', count(goals.id) as 'ゴール数' from goals left join players on goals.player_id = players.id group by players.position;
 
 /* 問9 */
+select birth, TIMESTAMPDIFF(YEAR, birth, '2014-06-13') as age, name, position from players order by age desc;
+
 /* 問10 */
+select SUM(CASE when g.player_id is NULL then 1 else 0 end) as COUNT(g.goal_time) from goals g;
+
 /* 問11 */
+select c.group_name, count(g.id) from goals g join pairings p on g.pairing_id = p.id and p.kickoff between '2014-6-13 00:00:00' and '2014-6-27 23:59:59' join countries c on p.my_country_id = c.id group by c.group_name;
+
 /* 問12 */
+select goals.goal_time from goals where 1=1 and pairing_id = 103;
+
 /* 問13 */
+select c.name, count(g.pairing_id) from goals g inner join pairings p on g.pairing_id = p.id and p.id in (103, 39) inner join countries c on p.my_country_id = c.id group by g.pairing_id, c.name;
+
 /* 問14 */
+select pa.kickoff, my_c.name as my_country, en_c.name as enemy_country, my_c.ranking as my_ranking, en_c.ranking as enemy_ranking, COUNT(g.id) from pairings pa left join countries my_c on pa.my_country_id = my_c.id left join countries en_c on pa.enemy_country_id = en_c.id left join goals g on pa.id = g.pairing_id
+where 1=1 and my_c.group_name = 'C' and en_c.group_name = 'C' group by pa.kickoff, my_c.name, en_c.name, my_c.ranking, en_c.ranking order by pa.kickoff, my_c.ranking;
+
 /* 問15 */
+select pa.kickoff, my_c.name as my_country, en_c.name as enemy_country, my_c.ranking as my_ranking, en_c.ranking as enemy_ranking, (select count(g.id) from goals g where pa.id = g.pairing_id) as my_goals from 
+pairings pa left join countries my_c on pa.my_country_id = my_c.id left join countries en_c on pa.enemy_country_id = en_c.id where 1=1 and my_c.group_name = 'C' and en_c.group_name = 'C' order by pa.kickoff, my_c.ranking;
+
 /* 問16 */
+select pa.kickoff, my_c.name as my_country, en_c.name as enemy_country, my_c.ranking as my_ranking, en_c.ranking as enemy_ranking, (select count(g.id) from goals g where pa.id = g.pairing_id) as my_goals, (select count(g2.id) from goals g2 left join pairings pa2 on pa2.id = g2.pairing_id where pa2.my_country_id = pa.enemy_country_id and pa2.enemy_country_id = pa.my_country_id) as enemey_goals from pairings pa left join countries my_c on pa.my_country_id = my_c.id left join countries en_c on pa.enemy_country_id = en_c.id where my_c.group_name = 'C' and en_c.group_name = 'C' order by pa.kickoff, my_c.ranking;
+
 /* 問17 */
+select pa.kickoff, my_c.name as my_country, en_c.name as enemy_country, my_c.ranking as my_ranking, en_c.ranking as enemy_ranking, (select count(g.id) from goals g where pa.id = g.pairing_id) as my_goals, (select count(g2.id) from goals g2 left join pairings pa2 on pa2.id = g2.pairing_id where pa2.my_country_id = pa.enemy_country_id and pa2.enemy_country_id = pa.my_country_id) as enemey_goals, (select count(g.id) from goals g where pa.id = g.pairing_id) - (select count(g2.id) from goals g2 left join pairings pa2 on pa2.id = g2.pairing_id and pa2.my_country_id = pa.enemy_country_id and pa2.enemy_country_id = pa.my_country_id) as 'goal diff' from pairings pa left join countries my_c on pa.my_country_id = my_c.id left join countries en_c on pa.enemy_country_id = en_c.id where my_c.group_name = 'C' and en_c.group_name = 'C' order by pa.kickoff, my_c.ranking;
+
 /* 問18 */
+select kickoff, date_add(kickoff, interval -12 hour) as kickoff_jp from pairings where 1=1 and my_country_id = 1 and enemy_country_id = 4;
+
 /* 問19 */
+select TIMESTAMPDIFF(YEAR, birth, '2014-06-13') as age, COUNT(id) as player_count from players group by age order by age asc;
+
 /* 問20 */
+select case when age between 10 and 19 then '10' when age between 20 and 29 then '20' when age between 30 and 39 then '30' when age between 40 and 49 then '40' end as age_group, count(*) from (select TIMESTAMPDIFF(YEAR, birth, '2014-06-13') as age from players) as ages group by age_group order by age asc;
+
 /* 問21 */
+select floor(TIMESTAMPDIFF(YEAR, birth, '2014-06-13') / 5) * 5 as age, count(id) from players group by age order by age asc;
+
 /* 問22 */
+select floor(TIMESTAMPDIFF(YEAR, birth, '2014-06-13') / 5) * 5 as age, position, count(id), AVG(height), AVG(weight) from players group by age, position order by age, position asc;
+
 /* 問23 */
+select name, height, weight from players order by height desc limit 5;
+
 /* 問24 */
+select name, height, weight from players order by height desc limit 15 offset 5;
